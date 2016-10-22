1e9ab3fde3769b79a1d68b0b3075c28f968d5773
cjfire <cjfire@sina.cn>
2016-10-22 16:14:18 +0800

    btn onclick method

--- DownloadPractise/app/src/main/java/com/weibo/cjfire/downloadpractise/MainActivity.java
+++ DownloadPractise/app/src/main/java/com/weibo/cjfire/downloadpractise/MainActivity.java
@@ -1,10 +1,15 @@
 package com.weibo.cjfire.downloadpractise;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.weibo.cjfire.downloadpractise.entities.FileInfo;
+import com.weibo.cjfire.downloadpractise.services.DownloadService;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +28,26 @@ public class MainActivity extends AppCompatActivity {
         mBtStop = (Button) findViewById(R.id.stop);
         mBtStart = (Button) findViewById(R.id.download);
 
+        final FileInfo fileInfo = new FileInfo(0,, "QQ.apk", "https://qd.myapp.com/myapp/qqteam/AndroidQQ/mobileqq_android.apk", 0, 0);
+
+        mBtStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DownloadService.class);
+                intent.setAction(DownloadService.ACTION_START);
+                intent.putExtra("fileInfo", fileInfo);
+                startService(intent);
+            }
+        });
+
+        mBtStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DownloadService.class);
+                intent.setAction(DownloadService.ACTION_STOP);
+                intent.putExtra("fileInfo", fileInfo);
+                startService(intent);
+            }
+        });
     }
 }
