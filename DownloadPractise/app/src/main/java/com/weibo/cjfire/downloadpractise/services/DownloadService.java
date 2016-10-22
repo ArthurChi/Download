9bc427f38af8d9e927671b0d4b8f9c2cb223d5b7
cjfire <cjfire@sina.cn>
2016-10-22 16:13:34 +0800

    create service class

---
+++ DownloadPractise/app/src/main/java/com/weibo/cjfire/downloadpractise/services/DownloadService.java
@@ -0,0 +1,10 @@
+package com.weibo.cjfire.downloadpractise.services;
+
+import android.app.Service;
+
+/**
+ * Created by cjfire on 16/10/22.
+ */
+
+public class DownloadService extends Service {
+}
