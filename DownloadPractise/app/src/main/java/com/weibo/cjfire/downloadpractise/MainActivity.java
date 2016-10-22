6d25d4081226f9a398a269b535f9405654027902
cjfire <cjfire@sina.cn>
2016-10-22 15:50:18 +0800

    main layout

--- DownloadPractise/app/src/main/java/com/weibo/cjfire/downloadpractise/MainActivity.java
+++ DownloadPractise/app/src/main/java/com/weibo/cjfire/downloadpractise/MainActivity.java
@@ -2,12 +2,26 @@ package com.weibo.cjfire.downloadpractise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTvFileName = null;
+    private ProgressBar mPbProgress = null;
+    private Button mBtStop = null;
+    private Button mBtStart = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTvFileName = (TextView) findViewById(R.id.fileName);
+        mPbProgress = (ProgressBar) findViewById(R.id.progressBar);
+        mBtStop = (Button) findViewById(R.id.stop);
+        mBtStart = (Button) findViewById(R.id.download);
+
     }
 }
--- DownloadPractise/app/src/main/java/com/weibo/cjfire/downloadpractise/entities/FileInfo.java
+++ DownloadPractise/app/src/main/java/com/weibo/cjfire/downloadpractise/entities/FileInfo.java
@@ -5,4 +5,50 @@ package com.weibo.cjfire.downloadpractise.entities;
  */
 
 public class FileInfo {
+
+    private int id;
+    private String url;
+    private String fileName;
+    private int length;
+    private int finished;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getFinished() {
+        return finished;
+    }
+
+    public void setFinished(int finished) {
+        this.finished = finished;
+    }
 }
